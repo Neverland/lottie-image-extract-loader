433fd5ed47566a68e04d773feeb20625f25eb593
enix <oncwnuFekZGnlchhdBPjoyyY3aEo@git.weixin.qq.com>
2021-12-01 15:13:39 +0800

    feat: init

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["lottie-image-extract-loader"] = factory());
+})(this, (function () { 'use strict';
+
+    /**
+     * @file index
+     * @author ienix(enix@foxmail.com)
+     *
+     * @since 2021/12/01
+     */
+    function loader(content, map, meta) {// console.log(content, map, meta);
+    }
+
+    return loader;
+
+}));
+//# sourceMappingURL=index.js.map
